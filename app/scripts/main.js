9707d6c59f465b2973173814f4587857eed8dcb9
JulianMayorga <mayorga.julian@gmail.com>
2013-07-09 18:34:41 -0300

    Add css transitions to elements. Add click handler on egg gun.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+var eggGun = $('#egg-gun');
+var egg = $('#egg');
+var troll = $('#troll');
+
+eggGun.click(function () {
+    egg.removeClass('notransition').addClass('fired');
+    eggGun.addClass('fired');
+
+    setTimeout(function () {
+        troll.addClass('dodging');
+    }, 1000);
+
+    setTimeout(function () {
+        troll.removeClass('dodging');
+        egg.addClass('notransition').removeClass('fired');
+        eggGun.removeClass('fired');
+    }, 2100);
+});
